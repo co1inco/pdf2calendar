4c0b4a5e0afaefcbd925c3bbeebab94454a04f0c
co1inco <colin.meihoefer@gmx.de>
2017-12-19 22:19:07 +0100

    load session from file

--- main.py
+++ main.py
@@ -3,12 +3,78 @@ from tkinter import messagebox
 
 import os
 
+global input
+
+class Checkbar(Frame): #https://www.python-kurs.eu/tkinter_checkboxes.php
+    def __init__(self, parent=None, picks=[], anchor=W):
+        Frame.__init__(self, parent)
+        self.vars = []
+        count = 0
+        for pick in picks:
+            var = IntVar()
+            chk = Checkbutton(self, text=pick, variable=var, bg = bgMain)
+            chk.grid(row=count, sticky=W)
+ 
+#           chk.pack( anchor=anchor, expand=YES, fill=X)
+            self.vars.append(var)
+            count = count + 1
+    def state(self):
+        return map((lambda var: var.get()), self.vars)
+
+
+def xlsxProcess(app):
+
+    text = Label(text="Filename:").pack()
+    inputText = Entry(app, width = 40)
+    inputText.pack()
+    button = Button(app, text="OK", command=lambda: useInput(app, inputText), width=10).pack()
+
+def useInput(app, inputText):
+    input = inputText.get()
+    app.destroy()
+
+    if os.path.isfile(input):
+
+        if input.endswith(".xlsx") or endswith(".xls"):
+             print("xlsx")
+             t = xlsx2name.readXlsx(input)
+             classes = t.getAllPages()
+             xlsx2name.writeToFile("classes.txt", classes)
+        else:
+            print("txt")
+            classes = getFileContent(input)
+
+        print(classes)
+
+        if loadgAPI:
+            createGoogleEntrys()
+
+    else:
+        messagebox.showerror("Warning", "unaple to locate file")
+
+def createGoogleEntrys():
+    pass
+
+
+
+def main():
+    
+    app = Tk()
+    app.title("pdf2cla")
+    app.geometry("250x80")
+
+    x = xlsxProcess(app)
+
+    app.mainloop()
+
+
+
+
+
 
 if __name__ == "__main__":
 
-    app = Tk()
-    app.title("pdf2cla")
-
+    global loadxrdp
     loadxrdp = False
     try:
         import xlrd
@@ -24,6 +90,7 @@ if __name__ == "__main__":
         else:
             messagebox.showerror("Warning", "Without xlrd you will be\nunable to load .xlsx files")
 
+    global loadgAPI
     loadgAPI = False
     try:
         import apiclient
@@ -45,3 +112,5 @@ if __name__ == "__main__":
     import xlsx2name
     if loadgAPI:
         import gCalendar
+    
+    main()

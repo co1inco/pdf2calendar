7e708f4e9ccdd07e6acf8481959895a202f71e0c
colinco <colin.meihoefer@gmx.de>
2018-10-24 11:27:11 +0200

    Include client_secret.json in final build

--- exe/setup.py
+++ exe/setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 import sys
 import os
 
-build_exe_options = {"packages": ["idna","tkinter"], "include_files": ["exe/tcl86t.dll", "exe/tk86t.dll"]}  
+build_exe_options = {"packages": ["idna","tkinter"], "include_files": ["exe/tcl86t.dll", "exe/tk86t.dll", "client_secret.json"]}  
 
 base = None  
 if sys.platform == "win32":  
